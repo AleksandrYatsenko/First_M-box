318da9cff957b936cec06bfb6581e98648fe76d3
Alex <yatsikxvii@gmail.com>
2022-06-23 15:41:41 +0300

    add 2nd slider

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ let slider = tns({
       items: 4
     },
     992: {
-      items: 5
+      items: 6
     },
     1200: {
       gutter: 24
@@ -27,12 +27,48 @@ let slider = tns({
   }
 });
 
-document.querySelector('.arrowPrev').addEventListener('click', function () {
+// let slider2 = tns({
+//   container: '.movies__slider',
+//   items: 6,
+//   nav: false,
+//   gutter: 27,
+//   controls: false,
+//   mouseDrag: true,
+//   responsive: {
+//     320: {
+//       items: 2,
+//       gutter: 16
+//     },
+//     576: {
+//       gutter: 20,
+//       items: 3
+//     },
+//     768: {
+//       items: 4
+//     },
+//     992: {
+//       items: 6
+//     },
+//     1200: {
+//       gutter: 24
+//     }    
+//   }
+// });
+
+document.querySelector('.showsPrev').addEventListener('click', function () {
   slider.goTo('prev')
 });
-document.querySelector('.arrowNext').addEventListener('click', function () {
+document.querySelector('.showsNext').addEventListener('click', function () {
   slider.goTo('next')
 });
+
+document.querySelector('.moviesPrev').addEventListener('click', function () {
+  slider.goTo('prev')
+});
+document.querySelector('.moviesNext').addEventListener('click', function () {
+  slider.goTo('next')
+});
+
 
 
 // document.addEventListener('DOMContentLoaded', () => {
