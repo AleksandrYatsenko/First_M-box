3d733efcdbbb2dcf793d2c7bf3f3774a0c6bbcfd
Alex <yatsikxvii@gmail.com>
2022-06-27 15:41:24 +0300

    add 3rd slider

--- js/script.js
+++ js/script.js
@@ -27,6 +27,13 @@ let slider = tns({
   }
 });
 
+document.querySelector('.showsPrev').addEventListener('click', function () {
+  slider.goTo('prev')
+});
+document.querySelector('.showsNext').addEventListener('click', function () {
+  slider.goTo('next')
+});
+
 let slider2 = tns({
   container: '.movies__slider',
   items: 6,
@@ -55,18 +62,46 @@ let slider2 = tns({
   }
 });
 
-document.querySelector('.showsPrev').addEventListener('click', function () {
-  slider.goTo('prev')
-});
-document.querySelector('.showsNext').addEventListener('click', function () {
-  slider.goTo('next')
-});
-
 document.querySelector('.moviesPrev').addEventListener('click', function () {
   slider2.goTo('prev')
 });
 document.querySelector('.moviesNext').addEventListener('click', function () {
   slider2.goTo('next')
+});
+
+let slider4 = tns({
+  container: '.actors__list',
+  items: 6,
+  nav: false,
+  gutter: 27,
+  controls: false,
+  mouseDrag: true,
+  responsive: {
+    320: {
+      items: 2,
+      gutter: 16
+    },
+    576: {
+      gutter: 20,
+      items: 3
+    },
+    768: {
+      items: 4
+    },
+    992: {
+      items: 6
+    },
+    1200: {
+      gutter: 24
+    }    
+  }
+});
+
+document.querySelector('.actorsPrev').addEventListener('click', function () {
+  slider4.goTo('prev')
+});
+document.querySelector('.actorsNext').addEventListener('click', function () {
+  slider4.goTo('next')
 });
 
 // -------- burger-menu ----------
