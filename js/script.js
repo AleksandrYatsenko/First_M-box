e47d6de5468ccf42021c1b0d9a2aa434905caa9a
Alex <yatsikxvii@gmail.com>
2022-06-29 14:36:52 +0300

    edit functions

--- js/script.js
+++ js/script.js
@@ -1,143 +1,165 @@
-
-let slider = tns({
-  container: '.shows__slider',
-  items: 6,
-  nav: false,
-  gutter: 27,
-  controls: false,
-  mouseDrag: true,
-  responsive: {
-    320: {
-      items: 2,
-      gutter: 16
-    },
-    576: {
-      gutter: 20,
-      items: 3
-    },
-    768: {
-      items: 4
-    },
-    992: {
-      items: 6
-    },
-    1200: {
-      gutter: 24
-    }    
+document.addEventListener('DOMContentLoaded', () => {
+  let slider = tns({
+    container: '.shows__slider',
+    items: 6,
+    nav: false,
+    gutter: 27,
+    controls: false,
+    mouseDrag: true,
+    responsive: {
+      320: {
+        items: 2,
+        gutter: 16
+      },
+      576: {
+        gutter: 20,
+        items: 3
+      },
+      768: {
+        items: 4
+      },
+      992: {
+        items: 6
+      },
+      1200: {
+        gutter: 24
+      }    
+    }
+  });
+  
+  document.querySelector('.showsPrev').addEventListener('click', function () {
+    slider.goTo('prev')
+  });
+  document.querySelector('.showsNext').addEventListener('click', function () {
+    slider.goTo('next')
+  });
+  
+  let slider2 = tns({
+    container: '.movies__slider',
+    items: 6,
+    nav: false,
+    gutter: 27,
+    controls: false,
+    mouseDrag: true,
+    responsive: {
+      320: {
+        items: 2,
+        gutter: 16
+      },
+      576: {
+        gutter: 20,
+        items: 3
+      },
+      768: {
+        items: 4
+      },
+      992: {
+        items: 6
+      },
+      1200: {
+        gutter: 24
+      }    
+    }
+  });
+  
+  document.querySelector('.moviesPrev').addEventListener('click', function () {
+    slider2.goTo('prev')
+  });
+  document.querySelector('.moviesNext').addEventListener('click', function () {
+    slider2.goTo('next')
+  });
+  
+  let slider4 = tns({
+    container: '.actors__list',
+    items: 6,
+    nav: false,
+    gutter: 27,
+    controls: false,
+    mouseDrag: true,
+    responsive: {
+      320: {
+        items: 2,
+        gutter: 16
+      },
+      576: {
+        gutter: 20,
+        items: 3
+      },
+      768: {
+        items: 4
+      },
+      992: {
+        items: 6
+      },
+      1200: {
+        gutter: 24
+      }    
+    }
+  });
+  
+  document.querySelector('.actorsPrev').addEventListener('click', function () {
+    slider4.goTo('prev')
+  });
+  document.querySelector('.actorsNext').addEventListener('click', function () {
+    slider4.goTo('next')
+  });
+  
+  // -------- burger-menu ----------
+  
+  const menuElem = document.querySelector('.header__nav'),
+        burgerElem = document.querySelector('.header__burger'),
+        menuItems = document.querySelectorAll('.header__link'),
+        undlayForNav = document.querySelector('.underlayerForNav');
+  
+  const openMenu = () => {
+    menuElem.classList.add('header__nav_active');
+    burgerElem.classList.add('burger_active');
+    undlayForNav.style.display = 'block';
   }
-});
-
-document.querySelector('.showsPrev').addEventListener('click', function () {
-  slider.goTo('prev')
-});
-document.querySelector('.showsNext').addEventListener('click', function () {
-  slider.goTo('next')
-});
-
-let slider2 = tns({
-  container: '.movies__slider',
-  items: 6,
-  nav: false,
-  gutter: 27,
-  controls: false,
-  mouseDrag: true,
-  responsive: {
-    320: {
-      items: 2,
-      gutter: 16
-    },
-    576: {
-      gutter: 20,
-      items: 3
-    },
-    768: {
-      items: 4
-    },
-    992: {
-      items: 6
-    },
-    1200: {
-      gutter: 24
-    }    
+  
+  const closeMenu = () => {
+    menuElem.classList.remove('header__nav_active');
+    burgerElem.classList.remove('burger_active');
+    undlayForNav.style.display = 'none';
   }
-});
-
-document.querySelector('.moviesPrev').addEventListener('click', function () {
-  slider2.goTo('prev')
-});
-document.querySelector('.moviesNext').addEventListener('click', function () {
-  slider2.goTo('next')
-});
-
-let slider4 = tns({
-  container: '.actors__list',
-  items: 6,
-  nav: false,
-  gutter: 27,
-  controls: false,
-  mouseDrag: true,
-  responsive: {
-    320: {
-      items: 2,
-      gutter: 16
-    },
-    576: {
-      gutter: 20,
-      items: 3
-    },
-    768: {
-      items: 4
-    },
-    992: {
-      items: 6
-    },
-    1200: {
-      gutter: 24
-    }    
-  }
-});
-
-document.querySelector('.actorsPrev').addEventListener('click', function () {
-  slider4.goTo('prev')
-});
-document.querySelector('.actorsNext').addEventListener('click', function () {
-  slider4.goTo('next')
-});
-
-// -------- burger-menu ----------
-
-const menuElem = document.querySelector('.header__nav');
-const burgerElem = document.querySelector('.header__burger');
-const menuItems = document.querySelectorAll('.header__link');
-
-burgerElem.addEventListener('click', () => {
-  menuElem.classList.toggle('header__nav_active');
-  burgerElem.classList.toggle('burger_active');
-});
-
-menuItems.forEach(item => {
-  item.addEventListener('click', () => {
-    menuElem.classList.toggle('header__nav_active');
-    burgerElem.classList.toggle('burger_active');
-  })
-});
-
-// ----------- modal  ---------------
-
-const over = document.querySelector('.overlay'),
-      modClose = over.querySelector('.modal__close'),
-      modName = over.querySelector('.modal__name'),
-      modBtnReg = over.querySelector('.modal__btn_register'),
-      modConfirmPass = over.querySelector('.modal__confirmPass'),
-      regBtn = document.querySelector('.register__btn'),
-      bodyOverflow = document.querySelector('body'),
-      headerSingIn = document.querySelector('.header__singIn');
-
+  
+  document.addEventListener('click', (e) => {
+    const targ = e.target;
+    if(targ.classList.contains('burger_active') || targ.classList.contains('underlayerForNav')) {
+      closeMenu();
+    }
+  });
+  
+  burgerElem.addEventListener('click', () => {
+    openMenu();
+    // menuElem.classList.toggle('header__nav_active');
+    // burgerElem.classList.toggle('burger_active');
+    // undlayForNav.style.display = 'block';
+  });
+  
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+      menuElem.classList.toggle('header__nav_active');
+      burgerElem.classList.toggle('burger_active');
+      undlayForNav.style.display = 'none';
+    });
+  });
+  
+  // ----------- modal  ---------------
+  
+  const over = document.querySelector('.overlay'),
+        modName = over.querySelector('.modal__name'),
+        modBtnReg = over.querySelector('.modal__btn_register'),
+        modConfirmPass = over.querySelector('.modal__confirmPass'),
+        regBtn = document.querySelector('.register__btn'),
+        bodyOverflow = document.querySelector('body'),
+        headerSingIn = document.querySelector('.header__singIn');
+  
   const openModal = () => {
     over.classList.remove('hidden');
     bodyOverflow.style.overflow = 'hidden';
   };
-
+  
   const closeModal = () => {
     over.classList.add('hidden');
     bodyOverflow.style.overflow = '';
@@ -147,7 +169,7 @@ const over = document.querySelector('.overlay'),
       modConfirmPass.classList.toggle('hidden');
     }
   };
-
+  
   headerSingIn.addEventListener('click', openModal);
   regBtn.addEventListener('click', openModal);
 
@@ -157,7 +179,7 @@ const over = document.querySelector('.overlay'),
       closeModal();
     }
   });
-
+  
   // ----------- btn "register" on modal ----------
   modBtnReg.addEventListener('click', () => {
     document.querySelector('.modal__reg').style.display = 'none';
@@ -169,17 +191,18 @@ const over = document.querySelector('.overlay'),
   document.querySelector('.modal__btn').addEventListener('click', (e) => {
     e.preventDefault();
   });
-
-//  ----------- Up -------------
-const elemUp = document.querySelector('.up');
-window.addEventListener('scroll', () => {
-  if(window.scrollY >= 1300) {
-    elemUp.classList.remove('hidden');
-    console.log(window.scrollY);
-  } else if (window.scrollY <= 1300) {
-    elemUp.classList.add('hidden');
-  }
+  
+  //  ----------- Up -------------
+  const elemUp = document.querySelector('.up');
+  window.addEventListener('scroll', () => {
+    if(window.scrollY >= 1300) {
+      elemUp.classList.remove('hidden');
+    } else if (window.scrollY <= 1300) {
+      elemUp.classList.add('hidden');
+    }
+  });
 });
+
 
   
 
